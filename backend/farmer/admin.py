2a08018e02b67e1c86fe58ccecc57212577b6593
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-02 12:06:48 -0500

    feat: admin for farmer and organizations

--- backend/farmer/admin.py
+++ backend/farmer/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from farmer.models import Farmer, Organization
+
 # Register your models here.
+
+@admin.register(Farmer)
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'is_active', 'user__email', 'organization__name')
+    search_fields = ('first_name', 'last_name', 'user__email')
+    list_filter = ('is_active', 'created_at', 'updated_at')
+    ordering = ('-created_at',)
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
