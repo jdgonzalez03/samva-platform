b4dae4f75fffb139872fe54bc9428b07d1cd7876
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-02 12:07:28 -0500

    feat: use wagtail_hook to access to models data

---
+++ backend/farmer/wagtail_hooks.py
@@ -0,0 +1,38 @@
+from django.utils.translation import gettext_lazy as _
+
+from farmer.models import Farmer, Organization
+from farmer.admin import FarmerAdmin, OrganizationAdmin
+
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
+
+
+class WagtailFarmerSnippetViewSet(SnippetViewSet):
+    model = Farmer
+    menu_label = _("Farmers")
+    icon = "user"
+    list_display = FarmerAdmin.list_display
+    search_fields = FarmerAdmin.search_fields
+    ordering = FarmerAdmin.ordering
+
+
+class WagtailOrganizationSnippetViewSet(SnippetViewSet):
+    model = Organization
+    menu_label = _("Organizations")
+    icon = "group"
+    list_display = OrganizationAdmin.list_display
+    search_fields = OrganizationAdmin.search_fields
+    ordering = OrganizationAdmin.ordering
+
+
+class WagtailFarmerSnippetViewSetGroup(SnippetViewSetGroup):
+    menu_label = _("Producers")
+    menu_icon = "folder-open-1"
+    menu_order = 200
+    items = (
+        WagtailFarmerSnippetViewSet,
+        WagtailOrganizationSnippetViewSet,
+    )
+
+
+register_snippet(WagtailFarmerSnippetViewSetGroup)
