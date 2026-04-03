b8df2731b0c5afabc688e8656defa478f1cdacd6
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-02 20:17:16 -0500

    feat: add vision & mision block

--- backend/cms/blocks.py
+++ backend/cms/blocks.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 from wagtail.blocks import StreamBlock, StructBlock, CharBlock, TextBlock, ChoiceBlock
 
-from cms.utils import ICON_CHOICES
+from cms.utils import ICON_CHOICES, BACKGROUND_CHOICES
 
 class HeroBlock(StructBlock):
     badge = CharBlock(
@@ -54,10 +54,49 @@ class HeroBlock(StructBlock):
         label = _("Hero")
     
 
+class CardBlock(StructBlock):
+    title = CharBlock(
+        required=True,
+        label=_("Título"),
+        help_text=_("Título de la tarjeta"),
+        default="Tarjeta",
+    )
+    description = TextBlock(
+        required=True,
+        label=_("Descripción"),
+        help_text=_("Descripción de la tarjeta"),
+        default="Descripción de la tarjeta",
+    )
+    icon = ChoiceBlock(
+        required=True,
+        choices=ICON_CHOICES,
+        default=ICON_CHOICES[0][0],
+        label=_("Icono"),
+        help_text=_("Icono de la tarjeta"),
+    )
+
+    class Meta:
+        icon = "table"
+        label = _("Tarjeta")
 
 
+class VisionMisionSectionBlock(StructBlock):
+    background = ChoiceBlock(
+        required=True,
+        choices=BACKGROUND_CHOICES,
+        default=BACKGROUND_CHOICES[0][0],
+        label=_("Fondo"),
+        help_text=_("Fondo de la sección"),
+    )
+    vision = CardBlock()
+    mision = CardBlock()
 
+    class Meta:
+        icon = "eye"
+        label = _("Visión y Misión")
+        
 
 
 class LandingStreamBlocks(StreamBlock):
     hero = HeroBlock()
+    vision_mision = VisionMisionSectionBlock()
