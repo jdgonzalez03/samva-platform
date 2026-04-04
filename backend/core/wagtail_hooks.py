7c5d4735277eada90da21d2d81de2cf9708c3864
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-04 17:11:57 -0500

    feat: add hook to inject leaflet static files

--- backend/core/wagtail_hooks.py
+++ backend/core/wagtail_hooks.py
@@ -7,3 +7,27 @@ def register_icons(icons):
         'core/icons/tractor.svg',
         'core/icons/organization.svg',
     ]
+
+# Mandatory Leaflet CSS and JS for Wagtail admin, if we use only django-admin, it doesnt need this
+@hooks.register('insert_global_admin_css')
+def leaflet_admin_css():
+    return """
+        <link rel="stylesheet" href="/static/leaflet/leaflet.css" />
+        <link rel="stylesheet" href="/static/leaflet/leaflet.extras.css" />
+        <link rel="stylesheet" href="/static/leaflet/draw/leaflet.draw.css" />
+        <style>
+            .leaflet-container-default {
+                height: 500px !important;
+                width: 100% !important;
+            }
+        </style>
+    """
+
+@hooks.register('insert_global_admin_js')
+def leaflet_admin_js():
+    return """
+        <script src="/static/leaflet/leaflet.js"></script>
+        <script src="/static/leaflet/draw/leaflet.draw.js"></script>
+        <script src="/static/leaflet/leaflet.extras.js"></script>
+        <script src="/static/leaflet/leaflet.forms.js"></script>
+    """
