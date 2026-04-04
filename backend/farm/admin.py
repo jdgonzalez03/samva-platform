732e647eb11b8a928d6a89e9a0739e9839b95a7f
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-04 17:12:39 -0500

    feat: add admin classes for farm and plot

--- backend/farm/admin.py
+++ backend/farm/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from farm.models import Farm, Plot
 # Register your models here.
+
+@admin.register(Farm)
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'address', 'created_at')
+    list_filter = ('owner', 'created_at')
+    search_fields = ('name', 'address')
+    ordering = ('-created_at',)
+
+
+@admin.register(Plot)
+class PlotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'farm', 'created_at')
+    list_filter = ('farm', 'created_at')
+    search_fields = ('name', 'farm__name')
+    ordering = ('-created_at',)
