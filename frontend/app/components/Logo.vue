4cb8116c8a2ddcf1f4243cfcf4eff41cf5f13ae6
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-04-20 17:10:22 -0500

    feat: add header

---
+++ frontend/app/components/Logo.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import tracktorIcon from '~/assets/icons/tracktor.svg'
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <img :src="tracktorIcon" alt="Tracktor" class="h-6 w-auto" />
+    <h1>S.A.M.V.A.</h1>
+  </div>
+</template>
