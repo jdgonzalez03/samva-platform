f3be944b9f78fd29905bf16336b0c2f68fb7a270
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-05-19 16:23:44 -0500

    feat: add login page

---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import * as z from 'zod';
+import LoginLayout from '~/layouts/LoginLayout.vue';
+import TractorIcon from '~/components/icons/TractorIcon.vue';
+
+import type { FormSubmitEvent, AuthFormField } from '@nuxt/ui';
+// TODO: Must be moved to a composable or a separate file to clean up the component and avoid unnecessary re-renders
+const fields: AuthFormField[] = [{
+  name: 'email',
+  label: 'Email',
+  type: 'email',
+  placeholder: 'Enter your email',
+  required: true
+}, {
+  name: 'password',
+  label: 'Password',
+  type: 'password',
+  placeholder: 'Enter your password',
+  required: true
+}]
+
+const schema = z.object({
+  email: z.email('Please enter a valid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters long')
+})
+
+type Schema = z.output<typeof schema>
+
+function onSubmit(payload: FormSubmitEvent<Schema>) {
+  console.log('Form submitted with values:', payload);
+}
+
+</script>
+
+<template>
+  <LoginLayout>
+    <UContainer class="min-h-screen flex flex-col items-center justify-center">
+      <UPageCard>
+        <UAuthForm
+          title="Login"
+          description="Enter your credentials to access your account."
+          :icon="TractorIcon"
+          :fields="fields"
+          @submit="onSubmit"
+        />
+      </UPageCard>
+    </UContainer>
+  </LoginLayout>
+</template>
