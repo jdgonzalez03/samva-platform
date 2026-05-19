1766a6474b674dc9282b81810e55a04cdc1302f3
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-05-19 16:25:04 -0500

    feat: add login layout

---
+++ frontend/app/layouts/LoginLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <UApp>
+    <div
+      class="fixed inset-0 bg-cover bg-center z-0"
+      style="background-image: url('/images/bg-login-page.svg')"
+    />
+
+    <UMain>
+      <slot />
+    </UMain>
+  </UApp>
+</template>
