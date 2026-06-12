725dbf3c1d81c0cfaad60e52ec777c1cd154e0f2
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-12 18:49:27 -0500

    feat: add auto field

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -70,6 +70,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'backend.urls'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
