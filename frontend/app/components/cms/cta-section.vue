2548000a39e23bcd5f10048ee22981c632429732
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-12 22:35:49 -0500

    feat: add cta section block

---
+++ frontend/app/components/cms/cta-section.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { CTASectionBlock } from '#shared/types/cms/blocks'
+import { getBlockIcon } from '#shared/utils/icons'
+
+interface Props {
+  block: CTASectionBlock
+}
+
+const props = defineProps<Props>()
+
+const buttonIcon = computed(() => getBlockIcon(props.block.value.cta_button.icon))
+</script>
+
+<template>
+  <section class="bg-primary py-16 w-screen">
+    <UContainer>
+      <div class="text-center max-w-3xl mx-auto">
+        <h2 class="text-4xl font-bold text-white">
+          {{ block.value.heading.title }}
+        </h2>
+
+        <p class="text-lg text-white/80 mt-4">
+          {{ block.value.heading.text }}
+        </p>
+
+        <UButton
+          color="primary"
+          :icon="buttonIcon"
+          :href="block.value.cta_button.url"
+          size="xl"
+          class="bg-white text-gray-900 hover:bg-gray-100 mt-8"
+        >
+          {{ block.value.cta_button.text }}
+        </UButton>
+      </div>
+    </UContainer>
+  </section>
+</template>
