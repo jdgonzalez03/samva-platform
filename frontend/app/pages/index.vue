62cba97b2504ba4f4c95e4d05b8e571100813a20
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-12 22:36:14 -0500

    refactor: update component imports and structure in index.vue

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -4,10 +4,6 @@ definePageMeta({
 })
 import { useRuntimeConfig } from '#imports'
 import type { LandingData } from '#shared/types/cms/landing'
-import VisionMision from '../components/cms/vision-mision.vue'
-import HeroSection from '../components/cms/hero-section.vue'
-import BenefitsSection from '../components/cms/benefits-section.vue'
-
 
 // TODO: Extract maybe to a composable or something, but for now it's fine here
 const config = useRuntimeConfig()
@@ -22,12 +18,14 @@ const { data } = await useFetch<LandingData>(
 </script>
 
 <template>
-  <div class="container flex flex-col items-center justify-center gap-12 px-4 py-4 mx-auto">
+  <div class="container flex flex-col items-center justify-center gap-12 px-4 py-4 pb-0 mx-auto">
     <template v-if="data?.body" v-for="block in data.body" :key="block.id">
-      <hero-section v-if="block.type === 'hero'" :block="block" />
-      <vision-mision v-else-if="block.type === 'vision_mision'" :block="block" />
-      <benefits-section v-else-if="block.type === 'benefits'" :block="block" />
+      <cms-hero-section v-if="block.type === 'hero'" :block="block" />
+      <cms-vision-mision v-else-if="block.type === 'vision_mision'" :block="block" />
+      <cms-benefits-section v-else-if="block.type === 'benefits'" :block="block" />
       <cms-team-section v-else-if="block.type === 'team'" :block="block" />
+      <cms-feature-highlight v-else-if="block.type === 'feature_highlight'" :block="block" />
+      <cms-cta-section v-else-if="block.type === 'cta_section'" :block="block" />
     </template>
   </div>
 </template>
