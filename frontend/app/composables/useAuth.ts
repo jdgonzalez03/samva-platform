25fc526c8dbbe96f8bac6174b9966781d311e057
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-16 12:03:36 -0500

    fix: remove unused file

--- frontend/app/composables/useAuth.ts
+++
@@ -1,8 +0,0 @@
-
-const isAuthenticated = useState('auth', () => false)
-// TODO: Implement actual authentication logic to set isAuthenticated based on user login status
-export function useAuth() {
-  return {
-    isAuthenticated: readonly(isAuthenticated)
-  }
-}
