ed59d38fdc391476ceea98ec9a93f4ab954a1c09
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-19 14:32:28 -0500

    feat: add new types to update profile

--- frontend/shared/types/accounts/profile.ts
+++ frontend/shared/types/accounts/profile.ts
@@ -1,3 +1,10 @@
+export interface OrganizationProfile {
+  id: number
+  name?: string
+  nit?: string
+  created_at?: string
+}
+
 export interface FarmerProfile {
   id: number
   first_name?: string
@@ -11,10 +18,25 @@ export interface FarmerProfile {
   address?: string
   avatar?: string
   is_active: boolean
+  organization?: OrganizationProfile | null
+  created_at?: string
 }
 
 export interface Profile {
   id: number
   email: string
   farmer: FarmerProfile
+}
+
+export interface UpdateProfilePayload {
+  first_name?: string, 
+  last_name?: string, 
+  document_type?: string,
+  document_number?: string,
+  gender?: string, 
+  phone_number?: string, 
+  city?: string,
+  department?: string, 
+  address?: string, 
+  avatar?: File
 }
