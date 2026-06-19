2bcfec3a72b860947c84fdef0c8585a398f331b0
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-19 16:35:46 -0500

    draft: add farms selector menu

---
+++ frontend/app/components/dashboard/FarmsMenu.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import type { DropdownMenuItem } from '@nuxt/ui'
+
+defineProps<{
+  collapsed?: boolean
+}>()
+
+const teams = ref([{
+  label: 'Planta el Sol',
+  avatar: {
+    src: 'https://github.com/nuxt.png',
+    alt: 'Nuxt'
+  }
+}, {
+  label: 'Granja Santa Rita',
+  avatar: {
+    src: 'https://github.com/nuxt-hub.png',
+    alt: 'NuxtHub'
+  }
+}, {
+  label: 'Planta Amarilo Llano Alto',
+  avatar: {
+    src: 'https://github.com/nuxtlabs.png',
+    alt: 'NuxtLabs'
+  }
+}])
+const selectedTeam = ref(teams.value[0])
+
+const items = computed<DropdownMenuItem[][]>(() => {
+  return [teams.value.map(team => ({
+    ...team,
+    onSelect() {
+      selectedTeam.value = team
+    }
+  }))]
+})
+</script>
+
+<template>
+  <UDropdownMenu
+    :items="items"
+    :content="{ align: 'center', collisionPadding: 12 }"
+    :ui="{ content: collapsed ? 'w-40' : 'w-(--reka-dropdown-menu-trigger-width)' }"
+  >
+    <UButton
+      v-bind="{
+        ...selectedTeam,
+        label: collapsed ? undefined : selectedTeam?.label,
+        trailingIcon: collapsed ? undefined : 'i-lucide-chevrons-up-down'
+      }"
+      color="neutral"
+      variant="ghost"
+      block
+      :square="collapsed"
+      class="data-[state=open]:bg-elevated"
+      :class="[!collapsed && 'py-2']"
+      :ui="{
+        trailingIcon: 'text-dimmed'
+      }"
+    />
+  </UDropdownMenu>
+</template>
