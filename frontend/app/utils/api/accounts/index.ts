998a8e13c202066ba6dbd648ad2b19b0806113fb
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-19 14:31:57 -0500

    feat: implement updateProfile endpoint

--- frontend/app/utils/api/accounts/index.ts
+++ frontend/app/utils/api/accounts/index.ts
@@ -1,6 +1,20 @@
 import { fetcher } from '../fetcher'
-import type { Profile } from '#shared/types/accounts/profile'
+import type { Profile, UpdateProfilePayload } from '#shared/types/accounts/profile'
 
 export const accountsApi = {
   getMe: () => fetcher.get<Profile>('accounts/me/'),
+  updateProfile: (payload: UpdateProfilePayload) => {
+    const hasFiles = payload.avatar
+
+    if (hasFiles) {
+      const formData = new FormData();
+      Object.entries(payload).forEach(([key, value]) => {
+        if (value !== undefined && value !== null && value !== "") {
+          formData.append(key, value);
+        }
+      });
+      return fetcher.patchFormData<Profile>('accounts/me/', formData)
+    }
+    return fetcher.patch<Profile>('accounts/me/', payload)
+  }
 }
