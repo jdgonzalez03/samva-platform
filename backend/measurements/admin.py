832748d71dcf8edfdbd31c6d0410d165ee848a5b
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-22 20:20:57 -0500

    feat: add measurement admin with wagtail hooks

--- backend/measurements/admin.py
+++ backend/measurements/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from measurements.models import SensorMeasurement, WeatherMeasurement, WeatherSnapshot
+
+
+@admin.register(WeatherSnapshot)
+class WeatherSnapshotAdmin(admin.ModelAdmin):
+    list_display = ('station', 'recorded_at', 'ingested_at')
+    search_fields = ('station__name',)
+    list_filter = ('station', 'recorded_at')
+    ordering = ('-recorded_at',)
+
+
+@admin.register(WeatherMeasurement)
+class WeatherMeasurementAdmin(admin.ModelAdmin):
+    list_display = ('snapshot', 'station_variable', 'value')
+    search_fields = ('station_variable__env_variable__name', 'station_variable__station__name')
+    list_filter = ('snapshot__station', 'station_variable__env_variable')
+    ordering = ('-snapshot__recorded_at',)
+
+
+@admin.register(SensorMeasurement)
+class SensorMeasurementAdmin(admin.ModelAdmin):
+    list_display = ('sensor_variable', 'value', 'recorded_at')
+    search_fields = ('sensor_variable__sensor__name', 'sensor_variable__env_variable__name')
+    list_filter = ('sensor_variable__sensor', 'sensor_variable__env_variable', 'recorded_at')
+    ordering = ('-recorded_at',)
