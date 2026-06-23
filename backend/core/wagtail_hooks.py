8e2081fa55db80cdb19efb77b07517a9333dc400
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-23 12:23:08 -0500

    feat(backend): add Flower task monitor to Wagtail admin menu

--- backend/core/wagtail_hooks.py
+++ backend/core/wagtail_hooks.py
@@ -31,3 +31,19 @@ def leaflet_admin_js():
         <script src="/static/leaflet/leaflet.extras.js"></script>
         <script src="/static/leaflet/leaflet.forms.js"></script>
     """
+
+
+from wagtail.admin.menu import MenuItem
+
+
+@hooks.register('register_admin_menu_item')
+def register_flower_menu_item():
+    return MenuItem(
+        'Task Monitor (Flower)',
+        'http://localhost:5555',
+        icon_name='cogs',
+        order=10000,
+        attrs={'target': '_blank', 'rel': 'noopener noreferrer'}
+    )
+# TODO: Add nginx as reverse proxy to serve /tasks/ pointing to
+#       the flower container on :5555, removing the localhost dependency
