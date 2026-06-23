e091487d4beb5f39807d504ab08e06310dbc2a71
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-06-23 16:28:41 -0500

    draft(backend): normalize response for other providers

--- backend/sensors/weather_station_providers/other.py
+++ backend/sensors/weather_station_providers/other.py
@@ -1,3 +1,11 @@
-def fetch_other():
+from django.utils import timezone
+
+
+def fetch_other(station):
     print('Fetching weather data from other sources')
     print('TODO: Please implement this function!')
+    return {
+        'data': {},
+        'raw': None,
+        'recorded_at': timezone.now(),
+    }
--- backend/sensors/weather_station_providers/wunderground.py
+++ backend/sensors/weather_station_providers/wunderground.py
@@ -1,3 +1,11 @@
-def fetch_wunderground():
+from django.utils import timezone
+
+
+def fetch_wunderground(station):
     print('Fetching weather data from wunderground')
     print('TODO: Please implement this function!')
+    return {
+        'data': {},
+        'raw': None,
+        'recorded_at': timezone.now(),
+    }
