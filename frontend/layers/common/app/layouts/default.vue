0773202f3b526b2abe6bac0f09af45249dffb36c
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-08-19 23:26:35 -0500

    feat: add default layout component with header, footer, and light color mode

---
+++ frontend/layers/common/app/layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
+const colorMode = useColorMode()
+
+onMounted(() => {
+  colorMode.preference = 'light'
+})
+</script>
+
+<template>
+  <div>
+    <Header />
+    <UMain>
+      <slot />
+    </UMain>
+    <Footer />
+  </div>
+</template>
