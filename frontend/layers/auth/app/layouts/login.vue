e3647e0ed949c30119e36e5df324b30824589c9b
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-08-20 17:31:19 -0500

    feat: add login page layout with color mode setup

---
+++ frontend/layers/auth/app/layouts/login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+onMounted(() => {
+  colorMode.preference = 'light'
+})
+</script>
+
+<template>
+  <div>
+    <div
+      class="fixed inset-0 bg-cover bg-center z-0"
+      style="background-image: url('/images/bg-login-page.svg')"
+    />
+
+    <UMain>
+      <slot />
+    </UMain>
+  </div>
+</template>
