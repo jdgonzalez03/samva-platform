2cca9a00d673775ba022d2731fe27061a90a5b80
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-08-22 17:21:02 -0500

    feat: extend farmApi to include getPlot and getWeather methods

--- frontend/layers/farm/app/utils/api/farm.ts
+++ frontend/layers/farm/app/utils/api/farm.ts
@@ -1,8 +1,11 @@
 import { fetcher } from '#api/fetcher'
-import type { Farm, Plot } from '../../types/farm'
+import type { Farm, FarmWeather, Plot, PlotDetail } from '../../types/farm'
 
 export const farmApi = {
   getFarms: () => fetcher.get<Farm[]>('farm/farms/'),
   getPlots: (farmId: number) =>
     fetcher.get<Plot[]>(`farm/farms/${farmId}/plots/`),
+  getPlot: (plotId: number) => fetcher.get<PlotDetail>(`farm/plots/${plotId}/`),
+  getWeather: (farmId: number) =>
+    fetcher.get<FarmWeather>(`farm/farms/${farmId}/weather/`),
 }
