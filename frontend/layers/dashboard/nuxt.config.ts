c0a787890f4f070b96fde5995cd2b8818ff9fad6
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-08-25 21:57:26 -0500

    feat: add imports configuration for composables in nuxt.config.ts

--- frontend/layers/dashboard/nuxt.config.ts
+++ frontend/layers/dashboard/nuxt.config.ts
@@ -1,5 +1,10 @@
+import { fileURLToPath } from 'url'
+
 export default defineNuxtConfig({
   $meta: { name: 'dashboard' },
+  imports: {
+    dirs: [fileURLToPath(new URL('./app/composables', import.meta.url))],
+  },
   i18n: {
     locales: [
       { code: 'es', file: 'es.json' },
