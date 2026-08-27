d8d816a42085395e63ddbcca765c649b992aa24f
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-08-27 17:38:41 -0500

    feat: add SensorsQueryKey enum for sensor data queries

---
+++ frontend/layers/sensors/app/constants/query-keys.ts
@@ -0,0 +1,8 @@
+export enum SensorsQueryKey {
+  ROOT = 'sensors',
+  HISTORY = 'history',
+  VARIABLES = 'variables',
+  READINGS = 'readings',
+  SERIES = 'series',
+  PLOT_AVERAGES = 'plotAverages',
+}
