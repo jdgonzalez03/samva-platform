5e4b6f7313f891963671da8c133fc117890d1736
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-08-27 17:25:15 -0500

    feat: add StatCard component for displaying dashboard statistics

---
+++ frontend/layers/dashboard/app/components/dashboard/StatCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  icon: string
+}>()
+</script>
+
+<template>
+  <UPageCard
+    :icon="icon"
+    :title="title"
+    variant="subtle"
+    :ui="{
+      container: 'gap-y-1.5',
+      wrapper: 'items-start',
+      leading:
+        'p-2.5 rounded-full bg-primary/10 ring ring-inset ring-primary/25 flex-col',
+      title: 'font-normal text-muted text-xs uppercase',
+    }"
+    class="lg:rounded-none lg:first:rounded-l-lg lg:last:rounded-r-lg hover:z-1"
+  >
+    <slot />
+  </UPageCard>
+</template>
